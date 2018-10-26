2e3f26d28a70df64f10c36c77143d2f73f6d16de
max <e@mail.ru>
2018-10-26 03:55:01 +0300

    initial service app

---
+++ proforient/service/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.models import User # заменен на EmailSignInUser
+from modelUtils.emailSignInModel import EmailSignInUser
+
+
+class CreateServiceForm(forms.Form):
+    title = forms.CharField(max_length=250)
+    description = forms.CharField(widget=forms.Textarea)
+    price = forms.IntegerField()
+
+    def clean_price(self):
+        if self.cleaned_data['price'] < 0:
+            raise forms.ValidationError('Цена не может быть меньше нуля:(')
+        return self.cleaned_data['price']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        return self.cleaned_data 
---
+++ proforient/service/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('services/', views.allServicesPage, name='allServices'),
+    path('category/<slug:category_name>/', views.servicesByCategoryPage, name='servicesByCategory'),
+    path('service/<int:id>', views.servicePage, name='servicePage'),
+    path('create_service/', views.createService, name='createService')
+]
