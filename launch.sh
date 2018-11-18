668192185a1eb33b9e4bfd8e961f6ffa49eece77
Ivan <ivan.morozov@list.ru>
2018-11-18 03:25:08 +0300

    Добавил: - Вывод по тегам - SideBar с тегами

---
+++ launch.sh
@@ -0,0 +1,7 @@
+python3 proforient/manage.py makemigrations admin 
+python3 proforient/manage.py makemigrations user 
+python3 proforient/manage.py makemigrations specialist 
+python3 proforient/manage.py makemigrations service 
+python3 proforient/manage.py makemigrations modelUtils 
+python3 proforient/manage.py migrate 
+python3 proforient/manage.py runserver 0.0.0.0:8000
